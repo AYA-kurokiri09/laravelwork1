49f56863bd21550babade67810e66a62d4d3bd81
AYA <60229621+AYA-kurokiri09@users.noreply.github.com>
2020-05-15 19:21:30 +0900

    Update Work1Controller.php

--- Work1Controller.php
+++ Work1Controller.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Response;
 
 class Work1Controller extends Controller
 {
-    //
+    public function index () {
+        return view('work1.index');
+    }
+
+    public function register () {
+        return view('work1.register');
+    }
+
 }
